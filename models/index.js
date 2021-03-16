afea7f64d4b36c1cc537bbc792256e07b9584467
Norbu Sherpa <norbuts@hotmail.com>
2021-03-16 22:53:20 +1100

    Updated Index.js file to have create

--- models/index.js
+++ models/index.js
@@ -1,36 +1,15 @@
 'use strict';
+const orm = require('../config/orm.js');
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
-
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+const cat = {
+  all(cb) {
+    orm.all('movies', (res) => cb(res));
+  },
+  // The variables cols and vals are arrays.
+  create(cols, vals, cb) {
+    orm.create('movies', cols, vals, (res) => cb(res));
   }
-});
+};
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+// Export the database functions for the controller (catsController.js).
+module.exports = movies;
