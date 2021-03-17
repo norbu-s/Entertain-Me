925b838ece2a2e4d17823ff205f40d49c5b24dac
Norbu Sherpa <norbuts@hotmail.com>
2021-03-17 22:18:10 +1100

    Update search fucntion

--- public/js/search.js
+++ public/js/search.js
@@ -3,7 +3,42 @@ document.addEventListener('DOMContentLoaded', (event) => {
       console.info('DOM loaded');
     }
 
-const searchButton = document.querySelectorAll('.searchMovie');
+const searchForm = document.querySelector('#home-form');
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // console.log("it's working")
+  const userInput = document.querySelector("#search-movie-home").value
+  fetch(`/api/searchmovies/${userInput}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+}).then((response) =>{
+  if (response.length > 0) {
+    console.log(response);
+  } else{
+    // api to imbd
+    fetch(`/api/cats/${id}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      //.then new api call to the servere to add movie to db
+      // dispolay result
+      // save to db
+  }
+})
+
+
+
+
+
+ 
+
+    const searchButton = document.querySelectorAll('.searchMovie');
 
       fetch(`/api/cats/${id}`, {
         method: 'GET',
@@ -38,7 +73,7 @@ if (createCatBtn) {
     };
 
     // Send POST request to create a new quote
-    fetch('/api/cats', {
+    fetch('/api/movies', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
