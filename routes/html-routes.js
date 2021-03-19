0e984903f06feced21e9c0408d448c472379301b
Norbu Sherpa <norbuts@hotmail.com>
2021-03-19 12:29:27 +1100

    update routes and APi call

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,35 +1,30 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
-const http = require("http");
+const https = require("https");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
-    console.log("I am here")
-    http 
-    .get("https://www.omdbapi.com/?t=romancing+the+stone&y=&plot=short&apikey=trilogy", res => {
-      let data ="";    
+    https
+    .get("https://www.omdbapi.com/?t=&plot=short&apikey=trilogy", res => {
+      let data ="";  
+      console.log("res",res)  
       res.on("data", chunk => {
         data += chunk;
+        console.log("chunk: ", chunk)
     });
   })
   res.on("end",()=> {
-    let url = JSON.parse(data).title;
-    console.log("chunk: ", chunk)
+    let url = JSON.parse(data);
   });
 });
 }
 
-
-  
-  
-  
-  
-  // {
-  //   if (req.length > 0) {
-  //     res.redirect("/index");
-  //   }
-  //   res.sendFile(path.join(__dirname, "../public/index.html"));
-  // });
+// {
+//   if (req.length > 0) {
+//     res.redirect("/index");
+//   }
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
 //   app.get("/movie", (req, res) => {
 //     if (req.movieTitle) {
