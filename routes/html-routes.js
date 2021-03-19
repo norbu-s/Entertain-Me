7c41607b2dd3e3735f69c8692e0f29821f064c08
Norbu Sherpa <norbuts@hotmail.com>
2021-03-19 11:44:26 +1100

    git push

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,32 +1,54 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
+const http = require("http");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
-    if (req.length > 0) {
-      res.redirect("/index");
-    }
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    console.log("I am here")
+    http 
+    .get("https://www.omdbapi.com/?t=romancing+the+stone&y=&plot=short&apikey=trilogy", res => {
+      let data ="";    
+      res.on("data", chunk => {
+        data += chunk;
+    });
+  })
+  res.on("end",()=> {
+    let url = JSON.parse(data).title;
+    console.log("chunk: ", chunk)
   });
+});
+}
 
-  app.get("/movie", (req, res) => {
-    if (req.movieTitle) {
-      res.redirect("/movieDetail");
-    }
-    res.sendFile(path.join(__dirname, "../public/movie.html"));
-  });
 
-  app.get("/watchList", (req, res) => {
-    if (req.watchList) {
-      res.redirect("/watchList");
-    }
-    res.sendFile(path.join(__dirname, "../public/watch.html"));
-  });
+  
+  
+  
+  
+  // {
+  //   if (req.length > 0) {
+  //     res.redirect("/index");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/index.html"));
+  // });
 
-  app.get("/reviewList", (req, res) => {
-    if (req.reviewList) {
-      res.redirect("/reviewList");
-    }
-    res.sendFile(path.join(__dirname, "../public/reviewList.html"));
-  });
-};
+//   app.get("/movie", (req, res) => {
+//     if (req.movieTitle) {
+//       res.redirect("/movieDetail");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/movie.html"));
+//   });
+
+//   app.get("/watchList", (req, res) => {
+//     if (req.watchList) {
+//       res.redirect("/watchList");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/watch.html"));
+//   });
+
+//   app.get("/reviewList", (req, res) => {
+//     if (req.reviewList) {
+//       res.redirect("/reviewList");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/reviewList.html"));
+//   });
+// }
