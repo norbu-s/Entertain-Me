c0384bbb5f89dfa7930a5f9f14be5608e5054f17
Norbu Sherpa <norbuts@hotmail.com>
2021-03-21 18:11:38 +1100

    Khai changes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,15 +7,15 @@ module.exports = (app) => {
 
   // index route loads view.html
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './app/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/blog.html'));
   });
 
   app.get('/cms', (req, res) => {
-    res.sendFile(path.join(__dirname, './app/public/review.html'));
+    res.sendFile(path.join(__dirname, './public/cms.html'));
   });
 
   // blog route loads blog.html
   app.get('/blog', (req, res) => {
-    res.sendFile(path.join(__dirname, './app/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/blog.html'));
   });
 };
