eac5121319de9db57c8c719325914056044aff4f
Norbu Sherpa <norbuts@hotmail.com>
2021-03-22 18:00:42 +1100

    Final Updates

--- models/review.js
+++ models/review.js
@@ -1,35 +1,40 @@
-
-  
-  module.exports = (sequelize, DataTypes) => {
-    const Review = sequelize.define('Review', {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1],
-        },
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define('Post', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
       },
-      review: {
+    },
+    review: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    rating: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        len: [1],
+        max: 5,
+        min: 1,
+      },
+    },
+      author: {
         type: DataTypes.TEXT,
         allowNull: false,
         validate: {
           len: [1],
-        },
-      },
-      rating: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          len: [1],
-        },
-      },
-      category: {
-        type: DataTypes.STRING,
+      },  
+    },
+    source: {
+      type: DataTypes.STRING,
+      defaultValue: 'Streaming',
+    }
+  });
 
-        defaultValue: 'cinema',
-
-      },
-    });
-    return Review;
-  };
-  
+  return Post;
+};
