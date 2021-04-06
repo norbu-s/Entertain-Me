dc2bc73974a4f9c894bcfa31bc5ff72cf792c567
suziestephen <suzietstephen@gmail.com>
2021-04-06 12:25:21 +1000

    title working

--- public/js/search.js
+++ public/js/search.js
@@ -1,14 +1,11 @@
-var searchText = $(".search-data")
-var moviesHistory = []
-var movie
-
+var searchText = $(".search-data");
+var moviesHistory = [];
+let movie;
 
 // Function to set movies from MoviesHistory array into local storage
 function saveMovies() {
-        localStorage.setItem("movies", JSON.stringify(moviesHistory));
+    localStorage.setItem("movies", JSON.stringify(moviesHistory));
 }
-
-
 
 // Function to render buttons based on what is in moviesHistory array
 function renderButtons() {
@@ -16,112 +13,93 @@ function renderButtons() {
     for (var i = 0; i < moviesHistory.length; i++) {
         var a = $("<button>");
         a.addClass("btn btn-danger movie-btn");
-        a.attr("data-Title", moviesHistory[i]);
+        a.attr("data-title", moviesHistory[i]);
         a.text(moviesHistory[i]);
         $(".buttons-view").prepend(a);
     }
 }
 
-
-
-
 // Function to display movie info
+
 function displayMovieInfo() {
-   
-    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
-    
+    const queryURL = "/api/search/" + movie;
     $.ajax({
         url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-            $(".search-data").html("")
+        method: "GET",
+    })
 
-            var movieDiv = $("<div class='movie'>")
-            movieDiv.html("<h4>You Want to Review</h4><br>")
-            searchText.prepend(movieDiv)
+    .then(function(response) {
+            $(".search-data").html("");
 
-            var imgURL = response.Poster;
-            var image = $("<img class='poster'>").attr("src", imgURL);
+            const movieDiv = $("<div class='movie'>");
+            movieDiv.html("<h4>You Want to Review</h4><br>");
+            searchText.prepend(movieDiv);
+
+            const imgURL = response.Poster;
+            const image = $("<img class='poster'>").attr("src", imgURL);
             movieDiv.append(image);
-          
-            var title = response.Title;
-            var pOne = $("<h2>").text(title);
-            movieDiv.append(pOne);
 
-            var genre = response.Genre;
-            var pTwo = $("<p>").text("Genre: " + genre);
-            movieDiv.append(pTwo);
-            var plot = response.Plot;
-            var pThree = $("<p>").text("Plot: " + plot);
-            movieDiv.append(pThree);
-            var director = response.Director;
-            var pFour = $("<p>").text("Director: " + director);
-            movieDiv.append(pFour);
-            var actors = response.Actors;
-            var pFive = $("<p>").text("Actors: " + actors);
-            movieDiv.append(pFive);
-            var year = response.Year;
-            var pSix = $("<p>").text("Year: " + year);
-            movieDiv.append(pSix);
-      
-          
-            if (moviesHistory.includes(response.Title) === false) {
-                moviesHistory.push(response.Title)
-            }
+            const title = response.title;
+            const pOne = $("<h2>").text(title);
+            movieDiv.append(pOne);
             
-            renderButtons()
-            saveMovies()
-        }).then((response) => {
-          if (response.length = 0)
-          fetch('/api/movies', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(review),
-          })
+            const genre = response.genre;
+            const pTwo = $("<p>").text("Genre: " + genre);
+            movieDiv.append(pTwo);
+            const plot = response.plot;
+            const pThree = $("<p>").text("Plot: " + plot);
+            movieDiv.append(pThree);
+            const director = response.director;
+            const pFour = $("<p>").text("Director: " + director);
+            movieDiv.append(pFour);
+            const actors = response.actors;
+            const pFive = $("<p>").text("Actors: " + actors);
+            movieDiv.append(pFive);
+            const year = response.year;
+            const pSix = $("<p>").text("Year: " + year);
+            movieDiv.append(pSix);
+
+            if (moviesHistory.includes(response.title) === false) {
+                moviesHistory.push(response.title);
+            }
+
+            renderButtons();
+            saveMovies();
         })
         .catch((error) => {
-          console.error('Error:', error);
+            console.error("Error:", error);
         });
-    };
-
-
+}
 
 //On click event listener for search button
-$("#run-search").on("click", function () {
-    movie = $("#search-term").val()
-    displayMovieInfo()
-
-})
+$("#run-search").on("click", function() {
+    movie = $("#search-term").val();
+    displayMovieInfo();
+});
 
 //On click event listener for movie buttons
-$(document).on("click", ".movie-btn", function () {
-    movie = $(this).attr("data-Title");
-    displayMovieInfo()
-
-})
+$(document).on("click", ".movie-btn", function() {
+    movie = $(this).attr("data-title");
+    displayMovieInfo();
+});
 
 //On click event listener for clear search results button
-$("#clear-search").on("click", function (){
-localStorage.clear("movies")
-moviesHistory = []
-$(".buttons-view").empty()
-//refresh page
-location.reload()
-})
-
-
-
+$("#clear-search").on("click", function() {
+    localStorage.clear("movies");
+    moviesHistory = [];
+    $(".buttons-view").empty();
+    //refresh page
+    location.reload();
+});
 
 //To run when document loads (if/else statement that will pull from local storage only if the value is not "null")
 $(document).ready(function() {
-    if(localStorage.getItem("movies") !== null) {
+    if (localStorage.getItem("movies") !== null) {
         var savedMovie = localStorage.getItem("movies");
-        var pushMovies = JSON.parse(savedMovie)
-        moviesHistory = moviesHistory.concat(pushMovies)
+        var pushMovies = JSON.parse(savedMovie);
+        moviesHistory = moviesHistory.concat(pushMovies);
     }
 
     //render buttons
-    renderButtons()
-  })
+    renderButtons();
+});
