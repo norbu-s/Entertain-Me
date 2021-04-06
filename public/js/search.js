f6ededc6f72c70d561b07a6f884783390ad543ce
suziestephen <suzietstephen@gmail.com>
2021-04-06 19:31:00 +1000

    adding in if call for array

--- public/js/search.js
+++ public/js/search.js
@@ -1,25 +1,23 @@
+
+
 var searchText = $(".search-data");
 var moviesHistory = [];
 let movie;
+
+
+
+
+
 
 // Function to set movies from MoviesHistory array into local storage
 function saveMovies() {
     localStorage.setItem("movies", JSON.stringify(moviesHistory));
 }
 
-// Function to render buttons based on what is in moviesHistory array
-function renderButtons() {
-    $(".buttons-view").empty();
-    for (var i = 0; i < moviesHistory.length; i++) {
-        var a = $("<button>");
-        a.addClass("btn btn-danger movie-btn");
-        a.attr("data-title", moviesHistory[i]);
-        a.text(moviesHistory[i]);
-        $(".buttons-view").prepend(a);
-    }
-}
+
 
 // Function to display movie info
+let searchTitle;
 
 function displayMovieInfo() {
     const queryURL = "/api/search/" + movie;
@@ -28,44 +26,59 @@ function displayMovieInfo() {
         method: "GET",
     })
 
+
     .then(function(response) {
             $(".search-data").html("");
+               
+            if(response[0]) {
+                searchTitle = response[0];
+            } else {
+                searchTitle = response;
+            }
 
             const movieDiv = $("<div class='movie'>");
             movieDiv.html("<h4>You Want to Watch</h4><br>");
             searchText.prepend(movieDiv);
 
-            const imgURL = response.Poster;
+            const imgURL = searchTitle.Poster;
             const image = $("<img class='poster'>").attr("src", imgURL);
             movieDiv.append(image);
 
-            const title = response.title;
+            const title = searchTitle.title;
             const pOne = $("<h2>").text(title);
             movieDiv.append(pOne);
             
-            const genre = response.genre;
+            const genre = searchTitle.genre;
             const pTwo = $("<p>").text("Genre: " + genre);
             movieDiv.append(pTwo);
-            const plot = response.plot;
+            const plot = searchTitle.plot;
             const pThree = $("<p>").text("Plot: " + plot);
             movieDiv.append(pThree);
-            const director = response.director;
+            const director = searchTitle.director;
             const pFour = $("<p>").text("Director: " + director);
             movieDiv.append(pFour);
-            const actors = response.actors;
+            const actors = searchTitle.actors;
             const pFive = $("<p>").text("Actors: " + actors);
             movieDiv.append(pFive);
-            const year = response.year;
+            const year = searchTitle.year;
             const pSix = $("<p>").text("Year: " + year);
             movieDiv.append(pSix);
 
-            if (moviesHistory.includes(response.title) === false) {
-                moviesHistory.push(response.title);
+
+ 
+            if (moviesHistory.includes(searchTitle.title) === false) {
+                moviesHistory.push(searchTitle.title);
             }
+
+
 
             renderButtons();
             saveMovies();
         })
+
+
+        
+
         .catch((error) => {
             console.error("Error:", error);
         });
