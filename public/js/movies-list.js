4df0101480db857f444fefa99a423521144160f2
suziestephen <suzietstephen@gmail.com>
2021-04-06 12:58:02 +1000

    fixing get call for movies buttons

--- public/js/movies-list.js
+++ public/js/movies-list.js
@@ -1,9 +1,11 @@
-//NOT USING THIS FILE CURRENTLY CAN MAYBE DELETE BUT WANT TO KEEP JUST IN CASE UNTIL SURE
+var searchText = $(".search-data")
+var moviesHistory = []
+// let movie;
 
-
-//var moviesHistory = []
-//var movie
-
+// Function to set movies from MoviesHistory array into local storage
+function saveMovies() {
+  localStorage.setItem("movies", JSON.stringify(moviesHistory));
+}
 
 
 
@@ -21,27 +23,70 @@ function renderButtons() {
 
 
 // Function to grab movies from the database
-const getMovie = (title) => {
-    fetch(`/api/movies/${title}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          console.log(`Success in grabbing movie ${title}`, data);
-        };
-    })
-}
+let title;
+
+function getTitle() {
+    const url = `/api/movies/${title}`;
+    $.ajax({
+            type: 'GET',
+            url: url,
+        })
+
+        .then(function(response) {
+          $("data-title").html("");
+
+          const movieDiv = $("<div class='movie'>");
+          movieDiv.html("<h4>You Want to Review</h4><br>");
+          searchText.prepend(movieDiv);
+
+          const imgURL = response.Poster;
+          const image = $("<img class='poster'>").attr("src", imgURL);
+          movieDiv.append(image);
+
+          const title = response.title;
+          const pOne = $("<h2>").text(title);
+          movieDiv.append(pOne);
+          
+          const genre = response.genre;
+          const pTwo = $("<p>").text("Genre: " + genre);
+          movieDiv.append(pTwo);
+          const plot = response.plot;
+          const pThree = $("<p>").text("Plot: " + plot);
+          movieDiv.append(pThree);
+          const director = response.director;
+          const pFour = $("<p>").text("Director: " + director);
+          movieDiv.append(pFour);
+          const actors = response.actors;
+          const pFive = $("<p>").text("Actors: " + actors);
+          movieDiv.append(pFive);
+          const year = response.year;
+          const pSix = $("<p>").text("Year: " + year);
+          movieDiv.append(pSix);
+
+
+
+        })
+
+      
+      };
+
+// //event listerner
+// $("btn btn-danger movie-btn").on("click", function() {
+//   title = $("#search-term").val()
+//   getTitle()
+
+// })
 
 
 //On click event listener for movie buttons
-$(document).on("click", ".movie-btn", function () {
-    movie = $(this).attr("data-title");
-    getMovie()
+// $(document).on("click", ".movie-btn", function () {
+//   title = $("data-title").val()
+//     getTitle()
 
+// });
+$(document).on("click", ".movie-btn", function () {
+  title = $(this).attr("data-title");
+  getTitle()
 })
 
 
@@ -52,10 +97,10 @@ $(document).ready(function() {
         var pushMovies = JSON.parse(savedMovie)
         moviesHistory = moviesHistory.concat(pushMovies)
     }
-})
 
 
 
     //render buttons
     renderButtons()
 
+  });
